a04d7d6224168cc8be3b2e4332952a33ee359cd2
8-sided-oregon <notebook23214@pm.me>
2023-09-24 20:02:24 -0400

    Added NavX2 implementation.

--- src/main/java/frc/robot/sensors/imu/NavX2.java
+++ src/main/java/frc/robot/sensors/imu/NavX2.java
@@ -1,15 +1,32 @@
 package frc.robot.sensors.imu;
 
+import com.kauailabs.navx.frc.AHRS;
+
 /**
  * Intermediary implementation and glue code for the NavX2 IMU. Instead of using
  * this directly, please use {@link IMU} instead.
  */
 public class NavX2 extends IMUBase {
-    public NavX2() {
+    private static NavX2 instance;
+
+    private AHRS ahrs;
+
+    private NavX2() {
+        // Idk how we actually connected the navx to the rio, soooo
+        // TODO: Figure out how it's actually connected and correct this.
+        this.ahrs = new AHRS();
+    }
+
+    public NavX2 getInstance() {
+        if (this.instance == null) {
+            this.instance = new NavX2();
+        }
+
+        return this.instance;
     }
 
     Orientation getOrientation() {
         // TODO: Implement me
-        return new Orientation(0, 0, 0);
+        return new Orientation(this.ahrs.getPitch(), this.ahrs.getFusedHeading(), this.ahrs.getRoll());
     }
 }
