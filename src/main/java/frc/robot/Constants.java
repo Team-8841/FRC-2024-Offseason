947899595b336547e05e2f1b84f3cda4a75e6304
8-sided-oregon <notebook23214@pm.me>
2023-09-24 21:20:37 -0400

    Added file for constants.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,25 @@ package frc.robot;
  * Various constants used throughout the program are defined here. They're
  * defined here instead of elsewhere just for ease of changing them.
  */
-public class Constants {
+public final class Constants {
+    // Constants for the bottom left swerve module.
+    public static final int SWERVE_BL_CANCODER_CAN_ID = 0;
+    public static final int SWERVE_BL_TALON_STEERING_CAN_ID = 1;
+    public static final int SWERVE_BL_TALON_DRIVE_CAN_ID = 2;
+
+    // Constants for the bottom right swerve module.
+    public static final int SWERVE_BR_CANCODER_CAN_ID = 3;
+    public static final int SWERVE_BR_TALON_STEERING_CAN_ID = 4;
+    public static final int SWERVE_BR_TALON_DRIVE_CAN_ID = 5;
+
+    // Constants for the top left swerve module.
+    public static final int SWERVE_TL_CANCODER_CAN_ID = 6;
+    public static final int SWERVE_TL_TALON_STEERING_CAN_ID = 7;
+    public static final int SWERVE_TL_TALON_DRIVE_CAN_ID = 8;
+
+    // Constants for the top right swerve module.
+    public static final int SWERVE_TR_CANCODER_CAN_ID = 9;
+    public static final int SWERVE_TR_TALON_STEERING_CAN_ID = 10;
+    public static final int SWERVE_TR_TALON_DRIVE_CAN_ID = 11;
 
 }
