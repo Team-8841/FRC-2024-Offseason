2f930f08c9fbf4bec35f86f9f472522760bc1aa0
8-sided-oregon <notebook23214@pm.me>
2023-09-24 21:11:58 -0400

    Added CTRE Pheonix vendor dep.

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+/**
+ * Various constants used throughout the program are defined here. They're
+ * defined here instead of elsewhere just for ease of changing them.
+ */
+public class Constants {
+
+}
