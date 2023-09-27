53305a7a8f7cfc59da10ef764a9b428af93cdeac
8-sided-oregon <notebook23214@pm.me>
2023-09-27 13:04:48 -0400

    Changed LED.java to LEDSubsystem.java.

--- src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
@@ -2,6 +2,6 @@ package frc.robot.subsystems.leds;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class LED extends SubsystemBase {
+public class LEDSubsystem extends SubsystemBase {
     // TODO: Implement me!
 }
