c25b51ffb9650f643f1713ac96175aeeec99ab71
8-sided-oregon <notebook23214@pm.me>
2023-10-08 16:57:23 -0400

    adjusted PlannedPath to use bezier curves

--- src/main/java/frc/robot/pathing/PlannedPath.java
+++ src/main/java/frc/robot/pathing/PlannedPath.java
@@ -3,23 +3,60 @@ package frc.robot.pathing;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Filesystem;
-import frc.robot.Constants;
+import frc.lib.math.CubicBezier;
 
 class PathAttributes {
     public List<PathWaypoint> waypoints;
     public List<PathMarker> markers;
+
+    @JsonCreator
+    public PathAttributes(@JsonProperty("waypoints") List<PathWaypoint> waypoints,
+            @JsonProperty("markers") List<PathMarker> markers) {
+        this.waypoints = waypoints;
+        this.markers = markers;
+    }
+}
+
+@JsonIgnoreProperties({"isReversal", "velOverride", "isLocked", "isStopPoint", "stopEvent"})
+class PathWaypoint {
+    Coordinate anchorPoint, prevControl, nextControl;
+    CubicBezier curve;
+    double holonomicAngle;
+
+    @JsonCreator
+    PathWaypoint(@JsonProperty("anchorPoint") Coordinate anchorPoint,
+            @JsonProperty("prevControl") Coordinate prevControl, @JsonProperty("nextControl") Coordinate nextControl,
+            @JsonProperty("holonomicAngle") double holonomicAngle) {
+        this.anchorPoint = anchorPoint;
+        this.prevControl = prevControl;
+        this.nextControl = nextControl;
+        this.holonomicAngle = holonomicAngle;
+    }
+
+    public CubicBezier getCurve() {
+        return this.curve;
+    }
+
+    public double getHolonomicAngle() {
+        return holonomicAngle;
+    }
 }
 
 class Coordinate {
     public double x, y;
 
-    public Coordinate(double x, double y) {
+    @JsonCreator
+    public Coordinate(@JsonProperty("x") double x, @JsonProperty("y") double y) {
         this.x = x;
         this.y = y;
     }
@@ -28,13 +65,12 @@ class Coordinate {
         this(translation.getX(), translation.getY());
     }
 
-    public Translation2d getTranslatin() {
+    public Translation2d getTranslation() {
         return new Translation2d(this.x, this.y);
     }
 }
 
 public class PlannedPath {
-    protected List<PathInstant> instants;
     protected List<PathWaypoint> waypoints;
     protected List<PathMarker> markers;
 
@@ -45,30 +81,34 @@ public class PlannedPath {
                 pathName, attributeFilePath);
 
         ObjectMapper mapper = new ObjectMapper();
-        
+
         PathAttributes parsedAttributes = mapper.readValue(attributes, PathAttributes.class);
-        this.waypoints = parsedAttributes.waypoints;
         this.markers = parsedAttributes.markers;
+        this.waypoints = parsedAttributes.waypoints;
 
-        System.out.format("Pathloader: Loaded path \"%s\" from \"%s\"\n"
-                + "Pathloader: num of waypoints: %d\n"
-                + "Pathloader: num of markers: %d\n",
-                pathName, attributeFilePath,
-                this.waypoints.size(), this.markers.size());
-        
-        this.calculateInstants();
-        this.normalizeInstants(this.instants.get(this.instants.size() - 1).time);
-    }
-
-    private void calculateInstants() {
-        for (int i = 1; i < this.waypoints.size(); i++) {
-            PathWaypoint lasWaypoint = this.waypoints.get(i - 1);
-            PathWaypoint curWaypoint = this.waypoints.get(i);
-
-            for (double t = 0; t <= 1; t += 1.0 / (Constants.pathInstantCount - 1)) {
-
+        List<Translation2d> splinePoints = new ArrayList<>();
+        for (PathWaypoint waypoint : parsedAttributes.waypoints) {
+            if (waypoint.prevControl != null) {
+                splinePoints.add(waypoint.prevControl.getTranslation());
+            }
+            splinePoints.add(waypoint.anchorPoint.getTranslation());
+            if (waypoint.nextControl != null) {
+                splinePoints.add(waypoint.nextControl.getTranslation());
             }
         }
+
+        for (int j = 0, i = 3; i < splinePoints.size(); j++, i += 3) {
+            Translation2d curvePoints[] = {
+                    splinePoints.get(i - 3),
+                    splinePoints.get(i - 2),
+                    splinePoints.get(i - 1),
+                    splinePoints.get(i),
+            };
+
+            this.waypoints.get(j).curve = new CubicBezier(curvePoints);
+        }
+
+        System.out.format("Pathloader: Loaded path \"%s\" from \"%s\"\n", pathName, attributeFilePath);
     }
 
     public static PlannedPath fetchPath(String pathName) {
@@ -84,22 +124,11 @@ public class PlannedPath {
         }
     }
 
-    private void normalizeInstants(double factor) {
-        for (int i = 0; i < this.instants.size(); i++) {
-            this.instants.get(i).time /= factor;
-        }
-    }
-
-    public List<PathInstant> getInstants() {
-        return this.instants;
+    public List<PathMarker> getPathMarkers() {
+        return this.markers;
     }
 
     public List<PathWaypoint> getPathWaypoints() {
         return this.waypoints;
     }
-
-    public List<PathMarker> getPathMarkers() {
-        return this.markers;
-    }
-
 }
